8373e5eb8dec0193b1522abfa27869a27e7f7ccc
Robert Wayne <robertjwayne@gmail.com>
2017-05-24 12:52:53 -0400

    Add functionality to edit a genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -12,4 +12,14 @@ class GenresController < ApplicationController
     @genre.save
     redirect_to genre_path(@genre)
   end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
 end
