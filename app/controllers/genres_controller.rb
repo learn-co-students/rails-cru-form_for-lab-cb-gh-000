54a6ce3a9b5e9d59406ef596111f58177847879d
Robert Wayne <robertjwayne@gmail.com>
2017-05-24 12:49:49 -0400

    Add functionality to create a genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,14 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(params.require(:genre).permit(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
 end
