c5695567fa6b6e5ea256c0cdb2c748f2158b95d7
Robert Wayne <robertjwayne@gmail.com>
2017-05-24 13:13:16 -0400

    Add functionality to create a new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,14 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(params.require(:song).permit(:name, :genre_id, :artist_id))
+    @song.save
+    redirect_to song_path(@song)
+  end
 end
