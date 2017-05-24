32f4509cc9ec979b8aef0d45b2075192d5c97697
Robert Wayne <robertjwayne@gmail.com>
2017-05-24 12:34:56 -0400

    Add functionality to edit an artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,4 +12,14 @@ class ArtistsController < ApplicationController
     @artist.save
     redirect_to artist_path(@artist)
   end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 end
